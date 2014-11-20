a0dee20e856b7948a045ad96e9c2edd1ed7ed854
Willem Thiart <himself@willemthiart.com>
2014-11-20 22:21:25 +0900

    Fixed tail bug

--- duraqueue.c
+++ duraqueue.c
@@ -352,8 +352,6 @@ int dqueue_poll(dqueue_t * me)
     item_t* item = arrayqueue_poll(me->items);
     me->head += item->len;
 
-    printf("me->head: %d %d\n", me->head, me->tail);
-
     free(item);
     if (me->size < me->head)
         me->head %= me->size;
